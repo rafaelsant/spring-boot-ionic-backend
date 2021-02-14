1901567dbd7fd32230c76b355d97dbbd2104c16a
Rafael Santiago <rafael.ribeiro.santiago@gmail.com>
2021-02-14 01:46:47 -0300

    adiciona relacionamentos

--- src/main/java/com/rafaelsantiago/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rafaelsantiago/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.rafaelsantiago.cursomc.resources;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rafaelsantiago.cursomc.domain.Categoria;
+import com.rafaelsantiago.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}" ,method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Optional<Categoria> obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
