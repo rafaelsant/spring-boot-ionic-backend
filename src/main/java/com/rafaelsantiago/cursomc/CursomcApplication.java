b222526d13b8b3c98e46ef2e3f084850ab151e71
Rafael Santiago <rafael.ribeiro.santiago@gmail.com>
2021-02-14 12:20:15 -0300

    Estado e Cidade

--- src/main/java/com/rafaelsantiago/cursomc/CursomcApplication.java
+++ src/main/java/com/rafaelsantiago/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rafaelsantiago.cursomc.domain.Categoria;
+import com.rafaelsantiago.cursomc.domain.Cidade;
+import com.rafaelsantiago.cursomc.domain.Estado;
 import com.rafaelsantiago.cursomc.domain.Produto;
 import com.rafaelsantiago.cursomc.repositories.CategoriaRepository;
+import com.rafaelsantiago.cursomc.repositories.CidadeRepository;
+import com.rafaelsantiago.cursomc.repositories.EstadoRepository;
 import com.rafaelsantiago.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository catRepo;
 	@Autowired
 	private ProdutoRepository prodRep;
+	@Autowired
+	private CidadeRepository cidRep;
+	@Autowired
+	private EstadoRepository estRep;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -33,6 +41,21 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p2 = new Produto(null,"Impressora",800.00);
 		Produto p3 = new Produto(null,"Mouse",20.00);
 		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		
+		Cidade c1 = new Cidade(null,"Uberlândia",est1);
+		Cidade c2 = new Cidade(null,"São Paulo",est2);
+		Cidade c3 = new Cidade(null,"Campinas",est2);
+		
+		System.out.println("*****************************************************************");
+		System.out.println(est1.getCidades());
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		System.out.println("*****************************************************************");
+		System.out.println(cat1.getProdutos());
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		
@@ -42,6 +65,8 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		catRepo.saveAll(Arrays.asList(cat1,cat2));
 		prodRep.saveAll(Arrays.asList(p1,p2,p3));
+		estRep.saveAll(Arrays.asList(est1,est2));
+		cidRep.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
