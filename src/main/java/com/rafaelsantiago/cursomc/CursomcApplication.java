35d015dd4d73a403ad52039d3787881ef4b33f47
Rafael Santiago <rafael.ribeiro.santiago@gmail.com>
2021-02-17 20:23:15 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/rafaelsantiago/cursomc/CursomcApplication.java
+++ src/main/java/com/rafaelsantiago/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.rafaelsantiago.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.rafaelsantiago.cursomc.domain.Cidade;
 import com.rafaelsantiago.cursomc.domain.Cliente;
 import com.rafaelsantiago.cursomc.domain.Endereco;
 import com.rafaelsantiago.cursomc.domain.Estado;
+import com.rafaelsantiago.cursomc.domain.Pagamento;
+import com.rafaelsantiago.cursomc.domain.PagamentoComBoleto;
+import com.rafaelsantiago.cursomc.domain.PagamentoComCartao;
+import com.rafaelsantiago.cursomc.domain.Pedido;
 import com.rafaelsantiago.cursomc.domain.Produto;
+import com.rafaelsantiago.cursomc.domain.enums.EstadoPagamento;
 import com.rafaelsantiago.cursomc.domain.enums.TipoCliente;
 import com.rafaelsantiago.cursomc.repositories.CategoriaRepository;
 import com.rafaelsantiago.cursomc.repositories.CidadeRepository;
 import com.rafaelsantiago.cursomc.repositories.ClienteRepository;
 import com.rafaelsantiago.cursomc.repositories.EnderecoRepository;
 import com.rafaelsantiago.cursomc.repositories.EstadoRepository;
+import com.rafaelsantiago.cursomc.repositories.PagamentoRepository;
+import com.rafaelsantiago.cursomc.repositories.PedidoRepository;
 import com.rafaelsantiago.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EnderecoRepository endRep;
 	@Autowired
 	private ClienteRepository cliRep;
+	@Autowired
+	private PedidoRepository pedRep;
+	@Autowired
+	private PagamentoRepository pagRep;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -43,6 +55,9 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY HH:mm");
+		
 		Categoria cat1 = new Categoria(null,"informática");
 		Categoria cat2 = new Categoria(null,"escritório");
 		
@@ -63,7 +78,17 @@ public class CursomcApplication implements CommandLineRunner {
 		Endereco e1 = new Endereco(null,"Rua Flores","300","Apto 203","Jardim","38777034",cli1,c1);
 		Endereco e2 = new Endereco(null,"Av Matos","105","Sala 800","Centro","38777012",cli1,c2);
 		
+		Pedido ped1 = new Pedido(null,sdf.parse("30/09/2017 10:32"),cli1,e1);
+		Pedido ped2 = new Pedido(null,sdf.parse("10/10/2020 10:12"),cli1,e1);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null,EstadoPagamento.QUITADO,ped1,6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null,EstadoPagamento.PENDENTE,ped2,sdf.parse("20/10/2017 00:00"),null);
+		ped2.setPagamento(pagto2);
+		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
 		
 		System.out.println("*****************************************************************");
 		System.out.println(est1.getCidades());
@@ -86,6 +111,8 @@ public class CursomcApplication implements CommandLineRunner {
 		cidRep.saveAll(Arrays.asList(c1,c2,c3));
 		cliRep.saveAll(Arrays.asList(cli1));
 		endRep.saveAll(Arrays.asList(e1,e2));
+		pedRep.saveAll(Arrays.asList(ped1,ped2));
+		pagRep.saveAll(Arrays.asList(pagto1,pagto2));
 	}
 
 }
--- src/main/java/com/rafaelsantiago/cursomc/domain/Cliente.java
+++ src/main/java/com/rafaelsantiago/cursomc/domain/Cliente.java
@@ -36,6 +36,9 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name = "telefone")
 	private Set<String> telefones = new HashSet<String>();
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<Pedido>();
 
 	public Cliente() {}
 
@@ -127,6 +130,14 @@ public class Cliente implements Serializable{
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 	
 }
